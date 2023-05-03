81d9904bf8ddfdf9bbbd19214cb7a7c4236c768d
Krunal-Sakhiya <krsakhiya204@gmail.com>
2023-05-03 16:22:58 +0530

    salesman module

---
+++ Model/Salesman/Address.php
@@ -0,0 +1,9 @@
+<?php
+class Model_Salesman_Address extends Model_Core_Table
+{
+	function __construct()
+	{
+		parent::__construct();
+		$this->setTableName('salesman_address')->setPrimarykey('address_id');
+	}
+}
